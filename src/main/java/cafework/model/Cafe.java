b350de8e7f03220d4c1b06ed0073b6593e44b3a4
ShizuNyanUwU <shizuru1806@gmail.com>
2026-05-21 10:05:31 +0700

    Fix Search function issues by adding Transient annotations for fields not in database

--- src/main/java/cafework/model/Cafe.java
+++ src/main/java/cafework/model/Cafe.java
@@ -3,19 +3,10 @@ package cafework.model;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.*;
 import org.hibernate.annotations.Formula;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
@@ -32,9 +23,9 @@ public class Cafe {
 
     private String name;
     
-    @Column(name = "owner_name")
+    @Transient //NOT A COLUMN IN DATABASE
     private String ownerName;
-    
+    @Transient //NOT A COLUMN IN DATABASE
     private String email;
     private String phone;
     
