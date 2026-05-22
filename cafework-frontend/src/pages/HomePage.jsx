425b0251b5f13c9a316691d8ed31553879c30ecd
TSho <Trung.PT235853@sis.hust.edu.vn>
2026-05-22 13:21:05 +0700

    feat(db) : create database seats

--- cafework-frontend/src/pages/HomePage.jsx
+++ cafework-frontend/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MapArea from '../components/MapArea';
 import SearchBar from '../components/Search/SearchBar'; 
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigation = useNavigate();
+  useEffect(() => {
+    const userRole = localStorage.getItem('role');
+    if(userRole === 'OWNER') {
+      navigation('/owner/dashboard');
+    }
+  }, [navigation]);
   const [cafes, setCafes] = useState([]);
   // KHO LƯU TRỮ CHỈ ĐƯỜNG: Nếu có dữ liệu thì hiện bảng chỉ đường, nếu null thì hiện SearchBar
   const [routeData, setRouteData] = useState(null);
